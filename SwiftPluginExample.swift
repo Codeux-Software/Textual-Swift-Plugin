969d14034604ffc76fb1d16685e98633644ecc58
Michael Morris <mikey@codeux.com>
2014-06-10 01:23:00 -0400

    first commit

---
+++ SwiftPluginExample.swift
@@ -0,0 +1,30 @@
+//
+//  PreferencePaneExample.swift
+//  PreferencePaneExample
+//
+//  Created by Michael Morris on 6/10/14.
+//
+//
+
+import Foundation
+
+class TPI_PreferencePaneExample: NSObject, THOPluginProtocol
+{
+	func pluginSupportsUserInputCommands() -> (NSArray)
+	{
+		return ["swift"]
+	}
+
+	func messageSentByUser(client: IRCClient, message: String, command: String)
+	{
+		let dateFormatter = NSDateFormatter()
+
+		dateFormatter.dateStyle = NSDateFormatterStyle.FullStyle
+		dateFormatter.timeStyle = NSDateFormatterStyle.FullStyle
+
+		let formattedDate = dateFormatter.stringFromDate(NSDate())
+		let formattedString = ("The current time is: " + formattedDate)
+
+		client.iomt().sendPrivmsgToSelectedChannel(formattedString)
+	}
+}
