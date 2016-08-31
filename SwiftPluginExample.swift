Codeux-Software/Textual-Swift-Plugin

commit a72f1c527b4f7999ea1a3173185d5db8c9989bac

Update project for Swift 3 and Textual 6

SwiftPluginExample.swift
@@ -9,36 +9,33 @@ It is appended to our new class object to inform Swift that we conform to it. */
 
 class TPI_SwiftPluginExample: NSObject, THOPluginProtocol
 {
-	var subscribedServerInputCommands: [AnyObject]! {
+	var subscribedServerInputCommands: [String] {
 		get {
 			return ["privmsg", "notice"]
 		}
 	}
 
-	func didReceiveServerInput(inputObject: THOPluginDidReceiveServerInputConcreteObject!, onClient client: IRCClient!)
+	func didReceiveServerInput(_ inputObject: THOPluginDidReceiveServerInputConcreteObject, on client: IRCClient)
 	{
 		/* Swift provides a very powerful switch statement so
 		it is easier to use that for identifying commands than
 		using an if statement if more than the two are added. */
-
-		NSLog("%@ %@", inputObject.messageCommand, inputObject.messageParamaters)
-
 		switch (inputObject.messageCommand) {
 			case "PRIVMSG":
-				self.handleIncomingPrivateMessageCommand(inputObject, onClient: client)
+				handleIncomingPrivateMessageCommand(inputObject, on: client)
 			case "NOTICE":
-				self.handleIncomingNoticeCommand(inputObject, onClient: client)
+				handleIncomingNoticeCommand(inputObject, on: client)
 			default:
 				return
 		}
 	}
 
-	func handleIncomingPrivateMessageCommand(inputObject: THOPluginDidReceiveServerInputConcreteObject!, onClient client: IRCClient!)
+	func handleIncomingPrivateMessageCommand(_ inputObject: THOPluginDidReceiveServerInputConcreteObject, on client: IRCClient)
 	{
 		/* Get message sequence of incoming message. */
-		let messageReceived = (inputObject.messageSequence as String)
+		let messageReceived = inputObject.messageSequence
 
-		let messageParamaters = (inputObject.messageParamaters as! Array<String>)
+		let messageParamaters = inputObject.messageParamaters
 
 		/* Get channel that message was sent from. */
 		/* The first paramater of the PRIVMSG command is always
@@ -46,12 +43,12 @@ class TPI_SwiftPluginExample: NSObject, THOPluginProtocol
 		let senderChannel = client.findChannel(messageParamaters[0])
 
 		/* Do not accept private messages. */
-		if senderChannel.isPrivateMessage {
+		if ((senderChannel?.isPrivateMessage) != nil) {
 			return
 		}
 
 		/* Get sender of message. */
-		let messageSender = (inputObject.senderNickname as String)
+		let messageSender = inputObject.senderNickname
 
 		/* Ignore this user, he's kind of a jerk. :-( */
 		if messageSender.hasPrefix("Alex") {
@@ -63,47 +60,47 @@ class TPI_SwiftPluginExample: NSObject, THOPluginProtocol
 			messageReceived == "does anybody know what time it is?")
 		{
 			/* Format message. */
-			let formattedString = (messageSender + ", the time where I am is: " + self.formattedDateTimeString())
+			let formattedString = (messageSender + ", the time where I am is: " + formattedDateTimeString())
 
 			/* Invoke the client on the main thread when sending. */
-			self.performBlockOnMainThread({
-				client.sendPrivmsg(formattedString, toChannel: senderChannel)
+			performBlock(onMainThread: {
+				client.sendPrivmsg(formattedString, to: senderChannel)
 			})
 		}
 	}
 
-	func handleIncomingNoticeCommand(inputObject: THOPluginDidReceiveServerInputConcreteObject!, onClient client: IRCClient!)
+	func handleIncomingNoticeCommand(_ inputObject: THOPluginDidReceiveServerInputConcreteObject, on client: IRCClient)
 	{
-		// Not implemented.
+		// Not implemented
 	}
 
 	/* Support a new command in text field. */
-	var subscribedUserInputCommands: [AnyObject]! {
+	var subscribedUserInputCommands: [String] {
 		get {
 			return ["datetime"]
 		}
 	}
 
-	func userInputCommandInvokedOnClient(client: IRCClient!, commandString: String!, messageString: String!)
+	func userInputCommandInvoked(on client: IRCClient, command commandString: String, messageString: String)
 	{
-		let formattedString = ("The current time is: " + self.formattedDateTimeString())
+		let formattedString = ("The current time is: " + formattedDateTimeString())
 
-		let mainWindow = self.masterController().mainWindow;
+		let mainWindow = masterController().mainWindow
 
-		self.performBlockOnMainThread({
-			client.sendPrivmsg(formattedString, toChannel:mainWindow.selectedChannel)
+		performBlock(onMainThread: {
+			client.sendPrivmsg(formattedString, to:mainWindow?.selectedChannel)
 		})
 	}
 
 	/* Helper functions. */
 	func formattedDateTimeString() -> (String)
 	{
-		let dateFormatter = NSDateFormatter()
+		let dateFormatter = DateFormatter()
 		
-		dateFormatter.dateStyle = NSDateFormatterStyle.FullStyle
-		dateFormatter.timeStyle = NSDateFormatterStyle.FullStyle
+		dateFormatter.dateStyle = DateFormatter.Style.fullStyle
+		dateFormatter.timeStyle = DateFormatter.Style.fullStyle
 		
-		let formattedDate = dateFormatter.stringFromDate(NSDate())
+		let formattedDate = dateFormatter.string(from: Date())
 		
 		return formattedDate
 	}

Info.plist
@@ -15,7 +15,7 @@
 	<key>CFBundleVersion</key>
 	<string>1.0.0</string>
 	<key>MinimumTextualVersion</key>
-	<string>5.0.0</string>
+	<string>6.0.0</string>
 	<key>NSPrincipalClass</key>
 	<string>TPI_SwiftPluginExample</string>
 </dict>

SwiftPluginExample.xcodeproj/project.pbxproj
@@ -115,7 +115,12 @@
 			isa = PBXProject;
 			attributes = {
 				LastSwiftUpdateCheck = 0700;
-				LastUpgradeCheck = 0700;
+				LastUpgradeCheck = 0800;
+				TargetAttributes = {
+					8D57630D048677EA00EA77CD = {
+						LastSwiftMigration = 0800;
+					};
+				};
 			};
 			buildConfigurationList = 1DEB911E08733D790010E9CD /* Build configuration list for PBXProject "SwiftPluginExample" */;
 			compatibilityVersion = "Xcode 3.2";
@@ -162,9 +167,9 @@
 		1DEB911B08733D790010E9CD /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
+				ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES = YES;
 				BUNDLE_LOADER = /Applications/Textual.app/Contents/MacOS/Textual;
 				COMBINE_HIDPI_IMAGES = YES;
-				EMBEDDED_CONTENT_CONTAINS_SWIFT = YES;
 				FRAMEWORK_SEARCH_PATHS = "\"/Applications/Textual.app/Contents/Frameworks/**\"";
 				HEADER_SEARCH_PATHS = "\"/Applications/Textual.app/Contents/Headers/**\"";
 				INFOPLIST_FILE = Info.plist;
@@ -172,6 +177,7 @@
 				PRODUCT_BUNDLE_IDENTIFIER = com.example.extension.SwiftPluginExample;
 				PRODUCT_NAME = SwiftPluginExample;
 				SWIFT_OBJC_BRIDGING_HEADER = "SwiftPluginExample-Bridging-Header.h";
+				SWIFT_VERSION = 3.0;
 				WRAPPER_EXTENSION = bundle;
 			};
 			name = Debug;
@@ -179,9 +185,9 @@
 		1DEB911C08733D790010E9CD /* Release */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
+				ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES = YES;
 				BUNDLE_LOADER = /Applications/Textual.app/Contents/MacOS/Textual;
 				COMBINE_HIDPI_IMAGES = YES;
-				EMBEDDED_CONTENT_CONTAINS_SWIFT = YES;
 				FRAMEWORK_SEARCH_PATHS = "\"/Applications/Textual.app/Contents/Frameworks/**\"";
 				HEADER_SEARCH_PATHS = "\"/Applications/Textual.app/Contents/Headers/**\"";
 				INFOPLIST_FILE = Info.plist;
@@ -189,6 +195,8 @@
 				PRODUCT_BUNDLE_IDENTIFIER = com.example.extension.SwiftPluginExample;
 				PRODUCT_NAME = SwiftPluginExample;
 				SWIFT_OBJC_BRIDGING_HEADER = "SwiftPluginExample-Bridging-Header.h";
+				SWIFT_OPTIMIZATION_LEVEL = "-Owholemodule";
+				SWIFT_VERSION = 3.0;
 				WRAPPER_EXTENSION = bundle;
 			};
 			name = Release;
@@ -196,7 +204,24 @@
 		1DEB911F08733D790010E9CD /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
+				CLANG_ANALYZER_LOCALIZABILITY_NONLOCALIZED = YES;
+				CLANG_WARN_BOOL_CONVERSION = YES;
+				CLANG_WARN_CONSTANT_CONVERSION = YES;
+				CLANG_WARN_EMPTY_BODY = YES;
+				CLANG_WARN_ENUM_CONVERSION = YES;
+				CLANG_WARN_INT_CONVERSION = YES;
+				CLANG_WARN_UNREACHABLE_CODE = YES;
+				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
+				ENABLE_STRICT_OBJC_MSGSEND = YES;
+				ENABLE_TESTABILITY = YES;
+				GCC_NO_COMMON_BLOCKS = YES;
 				GCC_OPTIMIZATION_LEVEL = 0;
+				GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
+				GCC_WARN_ABOUT_RETURN_TYPE = YES;
+				GCC_WARN_UNDECLARED_SELECTOR = YES;
+				GCC_WARN_UNINITIALIZED_AUTOS = YES;
+				GCC_WARN_UNUSED_FUNCTION = YES;
+				GCC_WARN_UNUSED_VARIABLE = YES;
 				ONLY_ACTIVE_ARCH = YES;
 			};
 			name = Debug;
@@ -204,6 +229,22 @@
 		1DEB912008733D790010E9CD /* Release */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
+				CLANG_ANALYZER_LOCALIZABILITY_NONLOCALIZED = YES;
+				CLANG_WARN_BOOL_CONVERSION = YES;
+				CLANG_WARN_CONSTANT_CONVERSION = YES;
+				CLANG_WARN_EMPTY_BODY = YES;
+				CLANG_WARN_ENUM_CONVERSION = YES;
+				CLANG_WARN_INT_CONVERSION = YES;
+				CLANG_WARN_UNREACHABLE_CODE = YES;
+				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
+				ENABLE_STRICT_OBJC_MSGSEND = YES;
+				GCC_NO_COMMON_BLOCKS = YES;
+				GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
+				GCC_WARN_ABOUT_RETURN_TYPE = YES;
+				GCC_WARN_UNDECLARED_SELECTOR = YES;
+				GCC_WARN_UNINITIALIZED_AUTOS = YES;
+				GCC_WARN_UNUSED_FUNCTION = YES;
+				GCC_WARN_UNUSED_VARIABLE = YES;
 			};
 			name = Release;
 		};

SwiftPluginExample.xcodeproj/xcshareddata/xcschemes/SwiftPluginExample.xcscheme
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Scheme
-   LastUpgradeVersion = "0700"
+   LastUpgradeVersion = "0800"
    version = "2.0">
    <BuildAction
       parallelizeBuildables = "YES"

commit 45819c4d4f68275edefd49944a4025e653a35e69

Fix incorrect condition introduced by a72f1c5

SwiftPluginExample.swift
@@ -43,7 +43,7 @@ class TPI_SwiftPluginExample: NSObject, THOPluginProtocol
 		let senderChannel = client.findChannel(messageParamaters[0])
 
 		/* Do not accept private messages. */
-		if ((senderChannel?.isPrivateMessage) != nil) {
+		if (senderChannel?.isPrivateMessage == false) {
 			return
 		}
 