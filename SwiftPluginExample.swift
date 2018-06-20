Codeux-Software/Textual-Swift-Plugin

commit 45819c4d4f68275edefd49944a4025e653a35e69

Fix incorrect condition introduced by a72f1c5

SwiftPluginExample.swift
@@ -43,7 +43,7 @@ class TPI_SwiftPluginExample: NSObject, THOPluginProtocol
 		let senderChannel = client.findChannel(messageParamaters[0])
 
 		/* Do not accept private messages. */
-		if ((senderChannel?.isPrivateMessage) != nil) {
+		if (senderChannel?.isPrivateMessage == false) {
 			return
 		}
 

commit 50c9b9e272c0cbf8d8bdd38acdfb07198e6cba7b

Fix incorrect condition introduced by 45819c4

SwiftPluginExample.swift
@@ -43,7 +43,7 @@ class TPI_SwiftPluginExample: NSObject, THOPluginProtocol
 		let senderChannel = client.findChannel(messageParamaters[0])
 
 		/* Do not accept private messages. */
-		if (senderChannel?.isPrivateMessage == false) {
+		if (senderChannel?.isChannel == false) {
 			return
 		}
 
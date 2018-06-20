50c9b9e272c0cbf8d8bdd38acdfb07198e6cba7b
Michael <michael@codeux.com>
2018-06-20 12:21:24 -0400

    Fix incorrect condition introduced by 45819c4

--- SwiftPluginExample.swift
+++ SwiftPluginExample.swift
@@ -43,7 +43,7 @@ class TPI_SwiftPluginExample: NSObject, THOPluginProtocol
 		let senderChannel = client.findChannel(messageParamaters[0])
 
 		/* Do not accept private messages. */
-		if (senderChannel?.isPrivateMessage == false) {
+		if (senderChannel?.isChannel == false) {
 			return
 		}
 
